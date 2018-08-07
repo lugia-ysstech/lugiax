96d5edffca8561438c0ade5ea90b0dcfa237e4eb
zenjava <138962816@qq.com>
2018-08-07 16:05:43 +0800

    修改：   * 增加测试用例。

--- packages/lugiax/src/test/index.test.js
+++ packages/lugiax/src/test/index.test.js
@@ -48,15 +48,14 @@ describe('lugiax', () => {
     oneModelCase();
   });
 
-  function oneModelCase() {
+  function createUserModel(name, pwd) {
     const model = 'user';
-    const name = 'ligx';
-    const pwd = '123456';
+
     const state = {
       name,
       pwd,
     };
-    const userModel = lugiax.register({
+    return lugiax.register({
       model,
       state,
       mutations: {
@@ -65,8 +64,19 @@ describe('lugiax', () => {
             return data.set('name', inParam.name);
           },
         },
+        async: {
+          async changePwd(data: Object, inParam: Object) {
+            return data.set('pwd', inParam.pwd);
+          },
+        },
       },
     });
+  }
+
+  function oneModelCase() {
+    const name = 'ligx';
+    const pwd = '123456';
+    const userModel = createUserModel(name, pwd);
     const MyInput = connect(
       Input,
       userModel,
@@ -94,5 +104,73 @@ describe('lugiax', () => {
     const name = 'hello new name';
     changeName({ name, });
     expect(getInputValue(target.find('input').at(0))).toBe(name);
+  });
+
+  it('connect only one model for async state change', async () => {
+    const { target, userModel, } = oneModelCase();
+    const {
+      mutations: { asyncChangePwd, },
+    } = userModel;
+    const pwd = '1234567';
+    await asyncChangePwd({ pwd, });
+    expect(getInputValue(target.find('input').at(1))).toBe(pwd);
+  });
+
+  it('connect only render by model "user" ', () => {
+    const name = 'ligx';
+    const pwd = 'helol';
+    const userModel = createUserModel(name, pwd);
+
+    const {
+      mutations: { changeInfo, },
+    } = lugiax.register({
+      model: 'infoModel',
+      state: {},
+      mutations: {
+        sync: {
+          changeInfo(data: Object, inParam: Object) {
+            return data.set('info', inParam.value);
+          },
+        },
+      },
+    });
+
+    let renderCnt = 0;
+
+    const MyInput = connect(
+      class extends React.Component<any> {
+        render() {
+          renderCnt++;
+          return [
+            <input value={this.props.name} />,
+            <input value={this.props.pwd} />,
+          ];
+        }
+      },
+      userModel,
+      (state: Object) => {
+        const { user, } = state;
+        return {
+          name: user.get('name'),
+          pwd: user.get('pwd'),
+        };
+      }
+    );
+    const target = mount(<MyInput />);
+    expect(renderCnt).toBe(1);
+    changeInfo({ value: 'helolo', });
+    expect(renderCnt).toBe(1);
+    const {
+      mutations: { changeName, },
+    } = userModel;
+    expect(getInputValue(target.find('input').at(0))).toBe(name);
+    expect(getInputValue(target.find('input').at(1))).toBe(pwd);
+    const newName = 'abcd';
+    changeName({ name: newName, });
+
+    expect(getInputValue(target.find('input').at(0))).toBe(newName);
+    expect(getInputValue(target.find('input').at(1))).toBe(pwd);
+
+    expect(renderCnt).toBe(2);
   });
 });
