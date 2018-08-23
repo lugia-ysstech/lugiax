cf9bfafe63d42a1a67f3dfb556282eebfd6930ab
zenjava <138962816@qq.com>
2018-08-23 20:33:03 +0800

    修改：   * 增加打包整体命令。

---
+++ tasks/dev.sh
@@ -0,0 +1,9 @@
+root_path=$PWD
+echo "$root_path"
+
+lugiacomplie="$root_path/node_modules/.bin/lugia-complie"
+
+cd "$root_path"/packages/lugiax-core
+$lugiacomplie dev --ig
+cd "$root_path"/packages/lugiax
+$lugiacomplie dev --ig
