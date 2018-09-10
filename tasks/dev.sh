51fdc845cd37b17f32062b7b4a0714f6c2dad7e2
zenjava <138962816@qq.com>
2018-09-10 12:59:53 +0800

    修改：   * 开发打包命令。

--- tasks/dev.sh
+++ tasks/dev.sh
@@ -4,6 +4,10 @@ echo "$root_path"
 lugiacomplie="$root_path/node_modules/.bin/lugia-complie"
 
 cd "$root_path"/packages/lugiax-core
-$lugiacomplie dev --ig
+$lugiacomplie dev --ig &
+
+cd "$root_path"/packages/lugiax-router
+$lugiacomplie dev --ig &
+
 cd "$root_path"/packages/lugiax
-$lugiacomplie dev --ig
+$lugiacomplie dev --ig &
