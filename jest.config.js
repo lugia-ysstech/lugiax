cc95af225183ef51c8d72967cbd355c6142c158b
zenjava <138962816@qq.com>
2020-02-14 21:53:25 +0800

    修改：   * jest的配置，解决windows无法正确执行测试用例的错误。

--- jest.config.js
+++ jest.config.js
@@ -4,12 +4,12 @@ module.exports = {
     '!src/**/demo*.js',
     '!src/*.js',
   ],
-  testMatch: ['<rootDir>/packages/**/?(*.)(spec|test).js?(x)'],
+  testMatch: [__dirname + '/packages/**/?(*.)(spec|test).js?(x)'],
   testEnvironment: 'node',
   testURL: 'http://localhost',
   transform: {
     '.+\\.(js|jsx)$':
-      '<rootDir>/node_modules/@lugia/mega-jest/lib/jsTransformer.js',
+      __dirname + '/node_modules/@lugia/mega-jest/lib/jsTransformer.js',
   },
   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
   moduleNameMapper: {
