39d6bd75b67bdc8b361d2cb410e89631cb1ffeaf
fjianzhou <fanjianzhou@ysstech.com>
2020-02-20 10:29:57 +0800

    新增: 	* 增加render文件。 	* 将lugiax的渲染与数据更新分离。 	* 减少mutations嵌套调用引起render次数。

--- packages/lugiax-core/test/render.sync.test.js
+++ packages/lugiax-core/test/render.sync.test.js
@@ -5,13 +5,118 @@
  * @flow
  */
 
-import Render from "../render";
+import render from "../src/render";
 
-describe("render.test.js", () => {
-  it("beginEnd", () => {
-    render.beginCall();
-    render.beginCall();
+describe("render.sync test", () => {
+  beforeEach(() => {
+    render.clear();
+  });
+  it("simple function use beginEnd", () => {
+    let count = 0;
+    render.onRender("a", () => {
+      ++count;
+    });
+    function a() {
+      render.beginCall("a");
+    }
+    a();
+    expect(Object.keys(render.preRenderModules).length).toBe(0);
+    expect(Object.keys(render.willRenderModules).includes("a")).toBe(true);
+    expect(Object.keys(render.willRenderModules).length).toBe(1);
+    expect(count).toBe(0);
   });
 
-  it("async", () => {});
+  it("simple function use beginEnd and endCall", () => {
+    let count = 0;
+    render.onRender("a", () => {
+      ++count;
+    });
+    function a() {
+      render.beginCall("a");
+      render.endCall();
+    }
+    a();
+    expect(Object.keys(render.preRenderModules).length).toBe(1);
+    expect(Object.keys(render.preRenderModules).includes("a")).toBe(true);
+    expect(Object.keys(render.willRenderModules).length).toBe(0);
+    expect(count).toBe(1);
+  });
+
+  it("simple function  use frist beginEnd and then endCall", () => {
+    let count = 0;
+    render.onRender("a", () => {
+      ++count;
+    });
+    function a() {
+      render.beginCall("a");
+    }
+    a();
+    expect(Object.keys(render.preRenderModules).length).toBe(0);
+    expect(Object.keys(render.willRenderModules).includes("a")).toBe(true);
+    expect(Object.keys(render.willRenderModules).length).toBe(1);
+    expect(count).toBe(0);
+    render.endCall();
+    expect(Object.keys(render.preRenderModules).length).toBe(1);
+    expect(Object.keys(render.preRenderModules).includes("a")).toBe(true);
+    expect(Object.keys(render.willRenderModules).length).toBe(0);
+    expect(count).toBe(1);
+  });
+
+  it("nesting function use beginEnd and endCall && Share one onRender", () => {
+    let count = 0;
+    render.onRender("a", () => {
+      ++count;
+    });
+    function a() {
+      render.beginCall("a");
+      b();
+      render.endCall();
+    }
+    function b() {
+      render.beginCall("a");
+      c();
+      render.endCall();
+    }
+    function c() {
+      render.beginCall("a");
+      render.endCall();
+    }
+    a();
+    expect(Object.keys(render.preRenderModules).length).toBe(1);
+    expect(Object.keys(render.preRenderModules).includes("a")).toBe(true);
+    expect(Object.keys(render.willRenderModules).length).toBe(0);
+    expect(count).toBe(1);
+  });
+
+  it("nesting function use beginEnd and endCall && Share more onRender", () => {
+    let count = 0;
+    render.onRender("a", () => {
+      ++count;
+    });
+    render.onRender("b", () => {
+      ++count;
+    });
+    render.onRender("c", () => {
+      ++count;
+    });
+    function a() {
+      render.beginCall("a");
+      b();
+      render.endCall();
+    }
+    function b() {
+      render.beginCall("b");
+      c();
+      render.endCall();
+    }
+    function c() {
+      render.beginCall("c");
+      render.endCall();
+    }
+    a();
+    expect(Object.keys(render.preRenderModules).length).toBe(3);
+    expect(Object.keys(render.preRenderModules)).toEqual(["a", "b", "c"]);
+    expect(Object.keys(render.willRenderModules).length).toBe(0);
+    expect(count).toBe(3);
+  });
 });
