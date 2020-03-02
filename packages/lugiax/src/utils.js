958d7f470bfd97e41fedff65d0c7fb9298f5ea8a
zenjava <138962816@qq.com>
2020-03-02 23:41:52 +0800

    重构代码：   * 将合并的公共方法提取到@lugia/combine包中。

--- packages/lugiax/src/utils.js
+++ packages/lugiax/src/utils.js
@@ -4,65 +4,9 @@
  *
  * @flow
  */
+import { combineFunction, } from '@lugia/combine';
 export function getDisplayName(Target: Object): string {
   return Target.displayName || Target.name || 'Component';
-}
-
-export function tillMethodAttribute(
-  paramA: Object
-): { [key: string]: Object[] } {
-  const events = {};
-  paramA &&
-    Object.keys(paramA).reduce((pre, current) => {
-      if (typeof paramA[current] === 'function') {
-        let config = pre[current];
-        if (!config) {
-          pre[current] = config = [];
-        }
-        config.push(paramA[current]);
-      }
-      return pre;
-    }, events);
-  return events;
-}
-
-export function combineMethodObject(...rest: Object[]): Object {
-  const res = {};
-  if (!rest) {
-    return res;
-  }
-
-  const valuesObjects = rest.map(param => tillMethodAttribute(param));
-
-  const keyObj = {};
-
-  valuesObjects.forEach(obj => {
-    Object.keys(obj).forEach(key => (keyObj[key] = true));
-  });
-
-  Object.keys(keyObj).forEach(key => {
-    const values = [];
-    valuesObjects.forEach(obj => {
-      const value = obj[key];
-      if (value) {
-        Array.prototype.push.apply(values, value);
-      }
-    });
-    res[key] = values;
-  });
-  return res;
-}
-
-export function combineFunction(...rest: Object[]): Object {
-  const combineObj = combineMethodObject(...rest);
-  const res = {};
-  Object.keys(combineObj).forEach(key => {
-    const method = combineObj[key];
-    res[key] = (...rest) => {
-      method && method.forEach(f => f(...rest));
-    };
-  });
-  return res;
 }
 
 export function withRef(enable: boolean, self: Object): Object {
@@ -82,11 +26,9 @@ export function isShouldRender(
   comparativeData: Object
 ): boolean {
   const areStatePropsEqualVal =
-    !areStatePropsEqual ||
-    areStatePropsEqual(comparativeData.preState, comparativeData.nextState);
+    !areStatePropsEqual || areStatePropsEqual(comparativeData.preState, comparativeData.nextState);
   const areOwnPropsEqualVal =
-    !areOwnPropsEqual ||
-    areOwnPropsEqual(comparativeData.preProps, comparativeData.nextProps);
+    !areOwnPropsEqual || areOwnPropsEqual(comparativeData.preProps, comparativeData.nextProps);
   return areStatePropsEqualVal && areOwnPropsEqualVal;
 }
-
+export { combineFunction };
