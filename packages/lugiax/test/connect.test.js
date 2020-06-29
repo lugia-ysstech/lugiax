25ec4285165be9a1b08e89982abdf6f9216c81f6
zenjava <138962816@qq.com>
2020-06-29 15:44:38 +0800

    修改测试用例:   * flow描述修改。

--- packages/lugiax/test/connect.test.js
+++ packages/lugiax/test/connect.test.js
@@ -397,7 +397,7 @@ describe('lugiax.connect', () => {
     let callCount = 0;
     const MyInput = connect(
       [userModel, infoModel,],
-      ([user, info]) => {
+      ([user, info,]) => {
         return {
           name: user.get('name'),
           pwd: user.get('pwd'),
